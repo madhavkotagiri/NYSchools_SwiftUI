e05ef4faebd6fa6687a78ee8855fef83b356a8d9
madhavkotagiri <madhavkotagiri1@gmail.com>
2023-03-03 18:43:12 -0500

    Initial Commit

---
+++ NYSchools_SwiftUI/NYSchools_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYSchools_SwiftUIApp.swift
+//  NYSchools_SwiftUI
+//
+//  Created by Madhav Kotagiri on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct NYSchools_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
